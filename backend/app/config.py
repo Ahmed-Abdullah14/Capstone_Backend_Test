a2bee39252994ea03900399f28cbcf533acf45d7
Ahmed-Abdullah14 <ahmed.abdullahsr16@gmail.com>
2026-03-09 20:59:22 -0600

    Updated .env.example, requirments.txt, kernel_config and config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
+OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
+
+#SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+#SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
+
+MANAGER_MODEL = os.getenv("MANAGER_MODEL", "openai/gpt-4o-mini")
+#PROFILER_MODEL = os.getenv("PROFILER_MODEL", "openai/gpt-4o-mini")   
+#CLASSIFIER_MODEL = os.getenv("CLASSIFIER_MODEL", "perplexity/sonar-pro")
+#CLUSTER_LABEL_MODEL = os.getenv("CLUSTER_LABEL_MODEL", "openai/gpt-4o")
+#CONTENT_MODEL = os.getenv("CONTENT_MODEL", "")
+
+
+#TEXT_EMBEDDING_MODEL = "text-embedding-3-small"
+#CLIP_MODEL = "openai/clip-vit-large-patch14"
+
+
+APP_NAME = "LumenIQ"
 
--- backend/app/kernel_config.py
+++ backend/app/kernel_config.py
@@ -4,23 +4,23 @@ from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 from app.config import OPENROUTER_API_KEY, OPENROUTER_BASE_URL, MANAGER_MODEL
 
 def kernel_init():
+    
+    # Initialize the semantic kernel instance
+    kernel = Kernel()
 
-  # Initialize the semantic kernel instance
-  kernel = Kernel()
+    # Initialize the OpenAI client
+    client = AsyncOpenAI(
+        api_key=OPENROUTER_API_KEY,
+        base_url=OPENROUTER_BASE_URL,
+    )
 
-  # Initialize the OpenAI client
-  client = AsyncOpenAI(
-    api_key = OPENROUTER_API_KEY,
-    base_url = OPENROUTER_BASE_URL,
-  )
+    # Create a chat completion service
+    chat_completion_service = OpenAIChatCompletion(
+        ai_model_id=MANAGER_MODEL,
+        async_client=client,
+    )
 
-  # Create a chat completion service
-  chat_completion_service = OpenAIChatCompletion(
-    ai_model_id = MANAGER_MODEL,
-    async_client = client,
-  )
-
-  # Add the service to the kernel
-  kernel.add_service(chat_completion_service)
-
-  return kernel
+    # Add the service to the kernel
+    kernel.add_service(chat_completion_service)
+    
+    return kernel
